f597ace17983d85e7a42993fff70f25ae0162271
JulianaCasta <julcastaeda11@misena.edu.co>
2021-11-04 16:58:14 -0500

    resta

--- JavaApplication11/src/javaapplication11/JavaApplication11.java
+++ JavaApplication11/src/javaapplication11/JavaApplication11.java
@@ -5,6 +5,8 @@
  */
 package javaapplication11;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Personal
@@ -15,10 +17,24 @@ public class JavaApplication11 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       //hola1
-        //hola2
-        //hola3
-        //Hola soy felipe
+    //diego
+    int num1;
+    int num2;
+    int resultado;
+    
+    Scanner constructor=new Scanner(System.in);
+    System.out.println("Ingrese el primer numero:");
+    num1=constructor.nextInt();
+    
+    System.out.println("Ingrese el segundo numero:");
+    num2=constructor.nextInt();
+    
+    resultado=num1+num2;
+    System.out.println("El resultado de la suma es "+resultado);
+    //juli
+    
+    resultado=num1-num2;
+    System.out.println("El resultado de la resta es "+resultado);
         
     }
     
